026b99144f76b1d18a7831e291ee70b493e8dcc1
Hajk <tunczorx@gmail.com>
2018-07-10 08:08:38 +0200

    Adding missing files.

--- LearningEnumerationTypesAndCollectionsV1.0/Form1.Designer.cs
+++ LearningEnumerationTypesAndCollectionsV1.0/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.yearBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.number = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Numer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "należał do gracza";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(358, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = ", który zakończył karierę w";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(221, 18);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.ReadOnly = true;
+            this.nameBox.Size = new System.Drawing.Size(131, 20);
+            this.nameBox.TabIndex = 4;
+            // 
+            // yearBox
+            // 
+            this.yearBox.Location = new System.Drawing.Point(498, 18);
+            this.yearBox.Name = "yearBox";
+            this.yearBox.ReadOnly = true;
+            this.yearBox.Size = new System.Drawing.Size(48, 20);
+            this.yearBox.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(563, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "roku";
+            // 
+            // number
+            // 
+            this.number.FormattingEnabled = true;
+            this.number.Location = new System.Drawing.Point(71, 19);
+            this.number.Name = "number";
+            this.number.Size = new System.Drawing.Size(36, 21);
+            this.number.TabIndex = 8;
+            this.number.SelectedIndexChanged += new System.EventHandler(this.number_SelectedIndexChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(616, 91);
+            this.Controls.Add(this.number);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.yearBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox yearBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox number;
     }
 }
 
